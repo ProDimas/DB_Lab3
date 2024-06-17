133149a281d828a67abca59f21b7222f46177c14
Dmytro Prokopiuk <dmitry22prokopyuk@gmail.com>
2024-06-17 21:34:04 +0300

    Getting connection to proper form

--- db/db_connection.py
+++ db/db_connection.py
@@ -1,2 +1,2 @@
-# CONNECTION_STRING = 'postgresql+psycopg2://postgres:123@localhost:5432/db_lab3'
-CONNECTION_STRING = 'mysql://root:1234@localhost/db_lab3'
+CONNECTION_STRING = 'postgresql+psycopg2://postgres:123@localhost:5432/db_lab3'
+# CONNECTION_STRING = 'mysql://root:1234@localhost/db_lab3'
